295ddedb9044cade6ec5a6fb7d56d3a356fb9f53
Jorge Medina <jorgemedinaleigh@protonmail.com>
2024-10-09 17:51:27 -0300

    First commit, Test Lippi API

---
+++ Test Lippi API/Test Lippi API/Data/API_DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test_Lippi_API.Data
+{
+    public class API_DbContext : DbContext
+    {
+        public API_DbContext(DbContextOptions<API_DbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ Test Lippi API/Test Lippi API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_Lippi_API.Models
+{
+    public class Product
+    {
+        public int productID { get; set; }
+        public required string productName { get; set; }
+        public int stock {  get; set; }
+        public decimal productPrice { get; set; }
+    }
+}
